ef7fcd7c2c9d198bf6ca2854da67f9ad24efa2e7
bogdang989 <bogdan.gavrilovic@sbgenomics.com>
2019-07-24 11:57:56 +0200

    Fix tests

--- tests/validator/test_cwl_validation.py
+++ tests/validator/test_cwl_validation.py
@@ -4,7 +4,7 @@ import sevenbridges
 from sbg_cwl_upgrader.validator.cwl_validation import CWLValidator
 import warnings
 import os
-import ruamel_yaml
+import yaml
 from sbg_cwl_upgrader.validator.sbg_validate_js_cwl_v1 import main
 
 
@@ -69,7 +69,7 @@ class TestCWLValidatorCLI(TestCase):
                                      'minimal-tool.cwl')
 
         with open(cls.test_file) as f:
-            cls.tool = ruamel_yaml.safe_load(f)
+            cls.tool = yaml.safe_load(f)
 
     @patch('logging.basicConfig', MagicMock())
     @patch('sbg_cwl_upgrader.validator.cwl_validation.CWLValidator',
@@ -115,7 +115,7 @@ class TestCWLValidatorValidate(TestCase):
                                      'minimal-tool.cwl')
 
         with open(cls.test_file) as f:
-            cls.tool = ruamel_yaml.safe_load(f)
+            cls.tool = yaml.safe_load(f)
 
         cls.wf = {
             "class": "Workflow",
